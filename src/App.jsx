23d265b020b0ec3735a09333cb00f7cb9228c20a
Semen Herasymiuk <semen.herasymiuk@gmail.com>
2025-04-11 20:48:47 +0600

    fixed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { PrivateRoute } from './components/PrivateRoute';
-import { RestrictedRoute } from './components/RestrictedRoute';
+import { Layout } from './components/Layout/Layout';
+import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
+import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoute';
 import { refreshUser } from './redux/auth/operations';
 import { useAuth } from './hooks';
 import { Toaster } from 'react-hot-toast';
 
-const HomePage = lazy(() => import('./pages/Home'));
-const RegisterPage = lazy(() => import('./pages/Register'));
-const LoginPage = lazy(() => import('./pages/Login'));
-const ContactsPage = lazy(() => import('./pages/Contacts'));
+const HomePage = lazy(() => import('./pages/Home/Home'));
+const RegisterPage = lazy(() => import('./pages/Register/Register'));
+const LoginPage = lazy(() => import('./pages/Login/Login'));
+const ContactsPage = lazy(() => import('./pages/Contacts/Contacts'));
 
 export const App = () => {
   const dispatch = useDispatch();
